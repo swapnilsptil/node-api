9e3abf5e640b1a9e72d65bca8f4b7c2427dcd23b
swapnilsptil <swapnils.ptil@gmail.com>
2018-07-04 22:38:40 +0530

    Creating develop branch and pushing basic skeleton

--- app.js
+++ app.js
@@ -2,10 +2,28 @@ const express = require('express');
 
 const app = express();
 
+const morgan = require('morgan')
+
 const productRouter = require('./api/routes/product');
 const orderRouter = require('./api/routes/orders')
 
+//routes to handle requests
 app.use('/product', productRouter);
 app.use('/orders', orderRouter);
+
+app.use(morgan('dev'));
+
+app.use((req, res, next) => {
+    const error = new Error('Not Found');
+    error.status(404);
+    next(error);
+})
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        message : error.message
+    })
+})
 
 module.exports = app;
