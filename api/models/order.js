c0b62ebb7369bd7a5fd51930b9d4d2247d9b347f
swapnilsptil <swapnils.ptil@gmail.com>
2018-07-08 13:48:33 +0530

    adding files

--- api/models/order.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const orderSchema = mongoose.Schema({
-    _id: mongoose.Schema.Types.ObjectId,
-    orderId: String,
-    quantity: Number
-})
-
-module.exports = mongoose.model('orderSchema', orderSchema);
--- api/models/product.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const productSchema = mongoose.Schema({
-    _id : mongoose.Schema.Types.ObjectId,
-    name : String,
-    price: {type: Number, required : [true,'price should placed']}
-}) 
-
-module.exports = mongoose.model('Product', productSchema);
